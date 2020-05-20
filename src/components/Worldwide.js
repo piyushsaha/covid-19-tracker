36e10ae042273e35a8b75b57c9b893e69dd4b961
Piyush Saha <itsmepiyushsaha@gmail.com>
2020-05-20 14:17:19 +0530

    added a message in the worldwide data page

--- src/components/Worldwide.js
+++ src/components/Worldwide.js
@@ -4,7 +4,7 @@ class Worldwide extends React.Component {
     render() {
         return (
             <div>
-
+                <h2>Covid-19 data from around the world will soon be added!</h2>
             </div>
         )
     }
