caebb5cdf0fc590fe6a023dc2178f829184a38a3
Piyush Saha <itsmepiyushsaha@gmail.com>
2020-05-20 14:46:41 +0530

    Updates

---
+++ precache-manifest.395584795fbd49052eb9d7caa765d7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cb556669835302d26b128d79832940",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "a7762b37af56a74b4036",
+    "url": "/covid-19-tracker/static/css/main.f921d657.chunk.css"
+  },
+  {
+    "revision": "f47b6d7c5e7b53183b86",
+    "url": "/covid-19-tracker/static/js/2.e34bcdc6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-19-tracker/static/js/2.e34bcdc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7762b37af56a74b4036",
+    "url": "/covid-19-tracker/static/js/main.4fd1956d.chunk.js"
+  },
+  {
+    "revision": "3d1c3a839b5135420af2",
+    "url": "/covid-19-tracker/static/js/runtime-main.94eb6b52.js"
+  }
+]);
