fda38c35bf9e95aa852a097489f952da1e221e80
Piyush Saha <itsmepiyushsaha@gmail.com>
2020-05-26 14:47:59 +0530

    imported and rendered the footer

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import Worldwide from "./components/Worldwide"
 import "./universal.css"
+import Footer from "./components/Footer"
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<HashRouter>
+				<HashRouter basename="/">
 					<Navbar />
 					<br /> <br />
 					<Switch>
-						<Route exact path="/" component={Home} />
+						<Route excat path="/" component={Home} />
 						<Route path="/worldwide" component={Worldwide} />
 					</Switch>
+					<Footer />
 				</HashRouter>
 			</div>
 		)
