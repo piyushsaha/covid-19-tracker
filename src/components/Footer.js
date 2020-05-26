17067bb4e2d8ecfbb477b98a44b25bb0e0657613
Piyush Saha <itsmepiyushsaha@gmail.com>
2020-05-26 14:38:20 +0530

    Added a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./footer.css"
+
+function Footer () {
+    return (
+        <div className="footer">
+            <span className="footer-text">Developed by Piyush &nbsp;&nbsp; | &nbsp;&nbsp; Data from <a href="https://www.covid19india.org/">covid19india.org</a></span>
+        </div>
+    )
+
+}
+
+export default Footer
